35ab1a588454e66d5556e5f712491e0a754ce144
rasoul asadian <rasoulub@gmail.com>
2024-12-08 21:01:02 +0330

    update demo

--- docs/styles.js
+++ docs/styles.js
@@ -1,3 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Add any JavaScript code here for interactivity
+// Image zoom modal functionality
+$(document).ready(function() {
+    $('.img-zoom').click(function() {
+        var imgSrc = $(this).data('src');
+        $('#zoomedImage').attr('src', imgSrc);
+        $('#imgModal').modal('show');
+    });
+
+    // Close modal on background click
+    $('#imgModal').on('hidden.bs.modal', function () {
+        $('#zoomedImage').attr('src', '');
+    });
 });
